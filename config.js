4bce44ed2daa0b2959df5e4db2aa430f129d0fff
HoangNV <viethoangnhu@gmail.com>
2025-04-13 18:23:50 +0700

    test

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    titleWeb: "Gửi em",
+
+    background: "background.jpg",
+
+    min1: "min1.jpeg",
+    min2: "min2.jpeg",
+    min3: "min3.jpeg",
+    min4: "min4.jpeg",
+    min5: "min5.jpeg",
+    min6: "min6.jpeg",
+
+    max1: "min1.jpeg",
+    max2: "min2.jpeg",
+    max3: "min3.jpeg",
+    max4: "min4.jpeg",
+    max5: "min5.jpeg",
+    max6: "max6.png", 
+}
